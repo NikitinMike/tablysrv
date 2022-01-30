fcf15756054be15a06fe5bacaa7ef34833e72a9d
mike <jobles11@gmail.com>
2022-01-30 21:14:03 +0300

    enable CORS

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,17 +2,20 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  row = [
-    1,
-    'jsdidhx',
-    'ghdhdhd',
-    'jfjfdjdfdsl',
-    356778,
-    'jdiow euhu wsuh',
-    'hdjfjikshf',
+  // rows = [
+  //   { key: 1, name: 'Вася', weigth: 11.1, tail: 101.55 },
+  //   { key: 2, name: 'Петя', weigth: 12.2, tail: 102.64 },
+  //   { key: 3, name: 'Жора', weigth: 13.3, tail: 103.73 },
+  //   { key: 4, name: 'Коля', weigth: 14.4, tail: 104.82 },
+  //   { key: 5, name: 'Лёша', weigth: 15.5, tail: 105.91 },
+  // ];
+  items = [
+    { id: 1, name: 'Яблоки', price: '$2' },
+    { id: 2, name: 'Персики', price: '$5' },
   ];
-  getHello(): any {
-    // return 'NESTJS SERVER APP <br/> Hello World!';
-    return [this.row, this.row, this.row];
+  getHello() {
+    // return 'Hello World!';
+    // return [this.row, this.row, this.row];
+    return { items: this.items };
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,26 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    allowedHeaders: [
+      'Cache-Control',
+      'Origin',
+      'X-Requested-With',
+      'Content-Type',
+      'Accept',
+      'X-Access-Token',
+      'x-api-token',
+      'Access-Control-Allow-Headers',
+      'Access-Control-Request-Method',
+      'Authorization',
+      'Authorized',
+    ],
+    credentials: true,
+    methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+    origin: '*',
+    maxAge: 60 * 60 * 24 * 365,
+    preflightContinue: false,
+  });
   await app.listen(3000);
 }
 bootstrap().then((r) => console.log(r));
