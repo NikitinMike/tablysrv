8ccf68d2043ecbbbb78d396aec143a8ecc3ee105
mike <jobles11@gmail.com>
2022-01-30 12:17:26 +0300

    test

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'NESTJS SERVER APP \n Hello World!';
+    return 'NESTJS SERVER APP <br/> Hello World!';
   }
 }
