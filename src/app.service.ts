7171e2a8bc534a7051b0932be168e54eeefdc036
mike <jobles11@gmail.com>
2022-01-30 14:43:06 +0300

    add data to service

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,17 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'NESTJS SERVER APP <br/> Hello World!';
+  row = [
+    '1',
+    'jsdidhx',
+    'ghdhdhd',
+    'jfjfdjdfdsl',
+    356778,
+    'jdiow euhu wsuh',
+    'hdjfjikshf',
+  ];
+  getHello(): any {
+    // return 'NESTJS SERVER APP <br/> Hello World!';
+    return [this.row, this.row, this.row];
   }
 }
