2aa97a722e63fecf1dba20b35fdd8565e98fe1b3
mike <jobles11@gmail.com>
2022-02-03 23:54:01 +0300

    config swagger fields api

--- src/contacts/contact.entity.ts
+++ src/contacts/contact.entity.ts
@@ -1,25 +1,33 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity()
 export class Contact {
+  @ApiProperty({})
   @PrimaryGeneratedColumn()
   id: number;
 
+  @ApiProperty({})
   @Column()
   firstName: string;
 
+  @ApiProperty({})
   @Column()
   lastName: string;
 
+  @ApiProperty({})
   @Column()
   email: string;
 
+  @ApiProperty({})
   @Column()
   phone: string;
 
+  @ApiProperty({})
   @Column()
   city: string;
 
+  @ApiProperty({})
   @Column()
   country: string;
 }
