479ff01a030eb7f122aee5edbc0f783b672e6d09
mike <jobles11@gmail.com>
2022-05-01 20:48:19 +0300

    jwt passport login as user/password

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,10 +2,16 @@ import { Controller, Get, Post, UseGuards, Request } from '@nestjs/common';
 import { AppService } from './app.service';
 import { AuthGuard } from '@nestjs/passport';
 import { LocalAuthGuard } from './auth/local-auth.guard';
+import { AuthService } from './auth/auth.service';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { Public } from './auth/constants';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private authService: AuthService,
+  ) {}
 
   @UseGuards(AuthGuard('local'))
   @Post('auth/login/old')
@@ -16,9 +22,17 @@ export class AppController {
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   async login(@Request() req) {
+    // return req.user;
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
     return req.user;
   }
 
+  @Public()
   @Get()
   getHello() {
     return this.appService.getHello();
