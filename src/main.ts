21c976309abaf9548247b5ef5d38f36b73851237
mike <jobles11@gmail.com>
2022-05-02 08:42:16 +0300

    implement https protocol cert

--- src/main.ts
+++ src/main.ts
@@ -8,24 +8,8 @@ import * as http from 'http';
 import * as express from 'express';
 
 async function bootstrap() {
-  // https://gist.github.com/cecilemuller/9492b848eb8fe46d462abeb26656c4f8
-  // https://sourceforge.net/projects/openssl-for-windows/
-  const httpsOptions = {
-    key: fs.readFileSync('C:\\OpenSSL\\SSL\\localhost.key', 'utf8'),
-    cert: fs.readFileSync('C:\\OpenSSL\\SSL\\localhost.crt', 'utf8'),
-  };
-
   const server = express();
   const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
-
-  const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
-    .setVersion('1.0')
-    .addTag('cats')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
 
   app.enableCors({
     allowedHeaders: [
@@ -49,7 +33,22 @@ async function bootstrap() {
   });
   await app.init();
 
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   http.createServer(server).listen(3000);
+  // https://gist.github.com/cecilemuller/9492b848eb8fe46d462abeb26656c4f8
+  // https://sourceforge.net/projects/openssl-for-windows/
+  const httpsOptions = {
+    key: fs.readFileSync('C:\\OpenSSL\\SSL\\localhost.key', 'utf8'),
+    cert: fs.readFileSync('C:\\OpenSSL\\SSL\\localhost.crt', 'utf8'),
+  };
   https.createServer(httpsOptions, server).listen(443);
 }
 
