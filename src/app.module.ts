0b7285c8e4aebe63efb430ccd2df9d25d5f9ca02
mike <jobles11@gmail.com>
2022-05-04 11:47:45 +0300

    global auth guard

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,6 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ContactsModule } from './contacts/contacts.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -15,8 +13,6 @@ import { UsersModule } from './users/users.module';
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
-    AuthModule,
-    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
