8b9473b7f8443d3d9ae0755f4c1f3fef40b65660
mike <jobles11@gmail.com>
2022-05-05 11:52:45 +0300

    eslint

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -11,6 +11,7 @@ export class AuthService {
 
   async validateUser(username: string, pass: string): Promise<any> {
     const user = await this.usersService.findOne(username);
+    console.log(user);
     if (user && user.password === pass) {
       const { password, ...result } = user;
       return result;
@@ -19,9 +20,11 @@ export class AuthService {
   }
 
   async login(user: any) {
-    const payload = { username: user.username, sub: user.userId };
     return {
-      access_token: this.jwtService.sign(payload),
+      access_token: this.jwtService.sign({
+        username: user.username,
+        sub: user.userId,
+      }),
     };
   }
 }
